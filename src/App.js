14bc5ea957ad0b90476900a07956a503a761b9fd
Yosuke Yamaguchi <yonosho456@gmail.com>
2020-08-12 00:57:06 +0900

    try class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
 
-function App() {
-  const greeting = "Hi tom";
-  const dom = <h1 className="foo">Hello, World! {greeting}</h1>;
-  const click = <input type="text" onClick={() => {console.log("clicked")}}/>;
-  const change = <input type="text" id="bar" onChange={() => {console.log("changed")}}/>;
-  const div = (// reactの制約で、返すのは1つのタグでないといけないので、divで囲む
-    <div>
-      <label htmlFor="bar">bar</label>
-      {change}
-    </div>
-  );
-  const fragment = (// ↑でdivいらないのになーを避けることができる
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      {change}
-    </React.Fragment>
-  );
-  return fragment;
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
 }
+
+const Cat = () => {
+  return (
+    <div>
+      <a>Meow</a>
+    </div>
+  )
+}
+
+// function App() {
+//   const greeting = "Hi tom";
+//   const dom = <h1 className="foo">Hello, World! {greeting}</h1>;
+//   const click = <input type="text" onClick={() => {console.log("clicked")}}/>;
+//   const change = <input type="text" id="bar" onChange={() => {console.log("changed")}}/>;
+//   const div = (// reactの制約で、返すのは1つのタグでないといけないので、divで囲む
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//       {change}
+//     </div>
+//   );
+//   const fragment = (// ↑でdivいらないのになーを避けることができる
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       {change}
+//     </React.Fragment>
+//   );
+//   return fragment;
+// }
 
 export default App;
