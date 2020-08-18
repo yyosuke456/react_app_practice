8bf15838c9b5c1b3330695c1e684cbf5d7539919
Yosuke Yamaguchi <yonosho456@gmail.com>
2020-08-18 22:21:21 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider} from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
